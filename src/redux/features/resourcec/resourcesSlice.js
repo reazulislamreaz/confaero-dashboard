77c2c72f91188a7e2533b0d56112d16654e74f7c
abusayedwd <110084857+abusayedwd@users.noreply.github.com>
2026-02-26 15:23:49 +0600

    recources slice create

---
+++ src/redux/features/resourcec/resourcesSlice.js
@@ -0,0 +1,41 @@
+import { Upload } from "antd";
+import { apiSlice } from "../../api/apiSlice";
+
+
+const resourcecSlice = apiSlice.injectEndpoints({
+    endpoints: (builder) => ({
+        ///// Resources
+        getDocuments: builder.query({
+            query: ({eventId,page, limit = 10}) => `/document/${eventId}/?page=${page}&limit=${limit}`,
+            providesTags: ['Resources']
+        }),
+
+        getDocumentDetails: builder.query({
+            query: ({eventId, id}) => `/document/${eventId}/details/${id}`,
+            providesTags: ['Resources']
+        }),
+
+        getPendingDocuments: builder.query({
+            query: ({eventId,}) => `document/${eventId}/pending?status=pending`,
+            providesTags: ['Resources']
+        }),
+
+
+
+
+        UploadDocument: builder.mutation({
+            query: ({ eventId, body }) => ({
+                url: `/resource/${eventId}`,
+                method: 'POST',
+                body: body,
+            }),
+            invalidatesTags: ['Resources'],
+        }),
+
+
+    }),});
+
+export const {
+    useGetResourcesQuery,
+    useCreateResourceMutation,
+} = resourcecSlice;
