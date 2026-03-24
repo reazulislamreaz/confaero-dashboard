c06123b192e1cc3116bb68012e383b985d79200b
reazulislam1487 <reazulislam1487@gmail.com>
2026-03-24 12:31:51 +0600

    chore: Update API base URL to use the local development server.

--- src/redux/api/baseUrl.js
+++ src/redux/api/baseUrl.js
@@ -1,3 +1,3 @@
-// const url = "http://10.10.11.30:8080";
-const url = "http://206.162.244.11:8078";
+const url = "http://10.10.11.30:8080";
+// const url = "http://206.162.244.11:8078";
 export default url;
