dee79dbac4c43aff99f69e5eaf337b6c5aca4ab0
reazulislam1487 <reazulislam1487@gmail.com>
2026-03-28 10:00:30 +0600

    added package

--- vite.config.js
+++ vite.config.js
@@ -3,22 +3,10 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    react(),
-  ],
-  // server: {
-  //   host: '0.0.0.0',
-  //   port: 3060,
-  //   allowedHosts: [
-  //     'sayedbro3060.syedbipul.me',
-  //     'reazul3060.suplify.life'
-  //   ],
-  //   open: true,
-  // },
+  plugins: [tailwindcss(), react()],
   server: {
-  host: '0.0.0.0',
-  port: 3060,
-  allowedHosts: 'all'
-}
+    host: true,
+    port: 3060,
+    allowedHosts: 'all'
+  }
 })
