0b4d7d93b93e9baac52141914bcc61184823ef65
reazulislam1487 <reazulislam1487@gmail.com>
2026-03-30 17:26:23 +0600

    feat: add verify email mutation endpoint to verifyEmailSlice

--- src/redux/features/verifyEmail/verifyEmailSlice.js
+++ src/redux/features/verifyEmail/verifyEmailSlice.js
@@ -15,6 +15,7 @@ const verifyEmailApiSlice = apiSlice.injectEndpoints({
             ]
           : [{ type: "VerifyEmail", id: "LIST" }],
     }),
+    // added
     addVerifyEmails: builder.mutation({
       query: ({ eventId, emails }) => ({
         url: `/organizer/verify-email/add`,
