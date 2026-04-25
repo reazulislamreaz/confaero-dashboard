e7489ae4ce45e46e3efbf65484ecccba96fc0d58
reazulislam1487 <reazulislam1487@gmail.com>
2026-04-25 10:08:52 +0600

    test comment

--- src/redux/api/apiSlice.js
+++ src/redux/api/apiSlice.js
@@ -8,6 +8,7 @@ const baseQuery = fetchBaseQuery({
     if (token) {
       headers.set("Authorization", `Bearer ${token}`);
     }
+    // test
 
     // ✅ Inject eventid from localStorage (as requested)
     const eventId = localStorage.getItem("selectedEventId");
