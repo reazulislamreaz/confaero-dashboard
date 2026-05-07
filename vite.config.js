8014e9ebec1159a42e3ccec7038f9a35ff9c2673
reazulislam1487 <reazulislam1487@gmail.com>
2026-05-07 16:43:49 +0600

    added test commit

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,32 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import tailwindcss from "@tailwindcss/vite";
+// export default defineConfig({
+//   plugins: [tailwindcss(), react()],
+//   server: {
+//     host: "3.150.217.155",
+//     port: 5000,
+//     allowedHosts: "all",
+//     // allowedHosts: [
+//     //   'sayedbro3060.syedbipul.me',
+//     //   'reazul3060.suplify.life'
+//     // ],
+//     open: true,
+//   },
+//   //   server: {
+//   //   host: '0.0.0.0',
+//   //   port: 3060,
+//   //   allowedHosts: 'all'
+//   // }
+// });
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
   plugins: [tailwindcss(), react()],
   server: {
-    host: "3.150.217.155",
+    host: "0.0.0.0",
     port: 5000,
-    allowedHosts: "all",
-    // allowedHosts: [
-    //   'sayedbro3060.syedbipul.me',
-    //   'reazul3060.suplify.life'
-    // ],
-    open: true,
   },
-  //   server: {
-  //   host: '0.0.0.0',
-  //   port: 3060,
-  //   allowedHosts: 'all'
-  // }
 });
