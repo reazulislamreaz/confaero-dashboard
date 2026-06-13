4d1c408b136218c5e4994ea34c0cebb043674bb2
reazulislam1487 <reazulislam1487@gmail.com>
2026-06-13 11:58:55 +0600

    fixed baseurl issues

--- src/redux/api/apiSlice.js
+++ src/redux/api/apiSlice.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { API_BASE_URL } from "./baseUrl";
-
+// this is a test
 const baseQuery = fetchBaseQuery({
   baseUrl: API_BASE_URL,
   prepareHeaders: (headers, { getState }) => {
